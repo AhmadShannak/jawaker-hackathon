2c221e4871aaef498e38d67de89a15f418973421
AhmadShannak <shannak.ahmad96@gmail.com>
2019-09-30 21:49:54 +0300

    adding fuel for timer + clean up on timeslow + reset

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,29 +5,34 @@ using UnityEngine.EventSystems;
 public class Controller : MonoBehaviour {
   [SerializeField]
   Player player;
-  Vector2 lastVelocity;
   Transform camera;
+  float pauseFuel = 30f;
+  bool outOfFuel = false;
+  bool useFuel = false;
+
   void Start() {
     this.transform.localScale = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 1.5f, Screen.height * 1.5f,Camera.main.nearClipPlane));
     camera = this.transform.parent;
   }
   void Update() {
-    
     if (player.transform.position.x >= this.transform.position.x) {
       camera.transform.position = new Vector3(player.transform.position.x, camera.transform.position.y, camera.transform.position.z);
     }
+    if (useFuel) {
+      pauseFuel -= Time.unscaledDeltaTime;
+      Debug.Log(pauseFuel.ToString("F"));
+    }
   }
+
   public void OnMouseDown() {
-    Jumpy.Time.timeScale = 0.15f;
-    lastVelocity = player.rb.velocity;
+    if(outOfFuel)
+      return;
+    useFuel = true;
     Jumpy.Time.SlowTime();
-    // player.rb.velocity = new Vector2;
-    // player.rb.gravityScale = 0;
   }
+
   public void OnMouseUp() {
-   Jumpy.Time.timeScale = 1;
-   Jumpy.Time.ReseTime();
-  //  player.rb.velocity = lastVelocity;
-  //  player.rb.gravityScale = 1;
+    useFuel = false;
+    Jumpy.Time.ReseTime();
   }
 }
--- Assets/Scripts/Time.cs
+++ Assets/Scripts/Time.cs
@@ -6,12 +6,13 @@ namespace Jumpy {
   public class Time : MonoBehaviour {
     public static float timeScale = 1;
     public static void SlowTime() {
+      Jumpy.Time.timeScale = 0.35f;
       UnityEngine.Time.timeScale = 0.05f;
       UnityEngine.Time.fixedDeltaTime = UnityEngine.Time.timeScale * 0.02f;
     }
     public static void ReseTime() {
+      Jumpy.Time.timeScale = 1;
       UnityEngine.Time.timeScale = 1;
-      // UnityEngine.Time.fixedDeltaTime = UnityEngine.Time.timeScale;
     }
   }
 }
