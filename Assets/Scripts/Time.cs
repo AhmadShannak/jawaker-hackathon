4f805c7e8f4a1af9433a8041bfe1d2a53b041ce7
“Fastkop” <fstkop@gmail.com>
2019-10-01 16:55:40 +0300

    level up logic added

--- Assets/Scripts/Time.cs
+++ Assets/Scripts/Time.cs
@@ -7,14 +7,14 @@ namespace Jumpy {
     public static float timeScale = 1;
 
     public static void SlowTime() {
-      Jumpy.Time.timeScale = 0.35f;
-      UnityEngine.Time.timeScale = 0.05f;
+      Jumpy.Time.timeScale = 0.55f;
+      UnityEngine.Time.timeScale = 0.05f ;
       UnityEngine.Time.fixedDeltaTime = UnityEngine.Time.timeScale * 0.02f;
     }
 
     public static void ReseTime() {
-      Jumpy.Time.timeScale = 1;
-      UnityEngine.Time.timeScale = 1;
+      Jumpy.Time.timeScale = 1 + Score.speed;
+      UnityEngine.Time.timeScale = 1 + Score.speed * 2f;
     }
   }
 }
