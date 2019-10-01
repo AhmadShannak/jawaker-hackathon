4014cb5a27a5b6f26b6ad5e59ee4ae958945dda6
Shannak <shannak.ahmad96@gmail.com>
2019-10-01 11:48:49 +0300

    timer for slowing down time

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,34 +5,49 @@ using UnityEngine.EventSystems;
 public class Controller : MonoBehaviour {
   [SerializeField]
   Player player;
+
   Transform camera;
-  float pauseFuel = 30f;
+  float pauseFuel = 15;
   bool outOfFuel = false;
   bool useFuel = false;
+  float maxFuel = 15;
 
   void Start() {
-    this.transform.localScale = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 1.5f, Screen.height * 1.5f,Camera.main.nearClipPlane));
+    this.transform.localScale = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 1.5f, Screen.height * 1.5f, Camera.main.nearClipPlane));
     camera = this.transform.parent;
   }
+
   void Update() {
     if (player.transform.position.x >= this.transform.position.x) {
       camera.transform.position = new Vector3(player.transform.position.x, camera.transform.position.y, camera.transform.position.z);
     }
-    if (useFuel) {
-      pauseFuel -= Time.unscaledDeltaTime;
-      Debug.Log(pauseFuel.ToString("F"));
-    }
+    Fuel();
   }
 
   public void OnMouseDown() {
-    if(outOfFuel)
+    if (outOfFuel)
       return;
     useFuel = true;
-    Jumpy.Time.SlowTime();
   }
 
   public void OnMouseUp() {
     useFuel = false;
-    Jumpy.Time.ReseTime();
   }
+
+  void Fuel() {
+    if (useFuel) {
+      if (pauseFuel > 0.2f) {
+        pauseFuel -= Time.unscaledDeltaTime;
+        Jumpy.Time.SlowTime();
+      } else {
+        useFuel = false;
+      }
+    } else {
+      if (pauseFuel < maxFuel) {
+        pauseFuel += Time.unscaledDeltaTime / 5;
+        Jumpy.Time.ReseTime();
+      }
+    }
+  }
+
 }
