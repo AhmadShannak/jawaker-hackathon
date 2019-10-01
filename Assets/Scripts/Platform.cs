bae804044de8775189b9a170fa105f5abc275190
Shannak <shannak.ahmad96@gmail.com>
2019-10-01 16:58:42 +0300

    main scnees + prefabs + platform script

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+  void Awake() {
+    this.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f,0.085f,10));
+  }
+}
