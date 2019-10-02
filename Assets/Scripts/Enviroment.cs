ac1bb4cf2ddd2fcbd99b7563ee86d48867fbe9b2
Shannak <shannak.ahmad96@gmail.com>
2019-10-02 21:11:50 +0300

    moving trees and clouds on different speeds

---
+++ Assets/Scripts/Enviroment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment : MonoBehaviour {
+  public static Vector3 outPos;
+  private void OnTriggerExit2D(Collider2D other) {
+    if (other.CompareTag("Left")) {
+      outPos = this.transform.parent.GetChild(2).localPosition;
+      outPos.x +=  18.3098f;
+      this.transform.localPosition = outPos;
+      this.transform.SetAsLastSibling();
+    }
+  }
+}
--- Assets/Scripts/Mountains.cs
+++ Assets/Scripts/Mountains.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mountains : MonoBehaviour {
-
+  [SerializeField]
+  float speed;
   // Update is called once per frame
   void Update() {
-    this.transform.Translate(new Vector3( -0.1f, 0, 0) * Jumpy.Time.timeScale);
+    this.transform.Translate(new Vector3( -speed, 0, 0) * Jumpy.Time.timeScale);
   }
 }
