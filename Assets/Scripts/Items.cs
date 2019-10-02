9b8d76dda230cf76075fec59a5d2d6b0ad66ab9b
Shannak <shannak.ahmad96@gmail.com>
2019-10-02 18:57:42 +0300

    cleaning

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Items : MonoBehaviour {
 
   void Update() {
-    this.transform.Translate(new Vector3( -0.1f, 0, 0) * Jumpy.Time.timeScale);
+    this.transform.Translate(new Vector3(-0.1f, 0, 0) * Jumpy.Time.timeScale);
   }
 
   private void OnTriggerEnter2D(Collider2D other) {
-  
+
     if (other.CompareTag("Player") && this.CompareTag("Time")) {
-            GameObject.Find("BG").GetComponent<Controller>().ActivateAddTime();
-            Destroy(this.gameObject);
+      GameObject.Find("BG").GetComponent<Controller>().ActivateAddTime();
+      Destroy(this.gameObject);
     } else if (other.CompareTag("Player") && this.CompareTag("Shield")) {
       // Give Shield
     } else if (other.CompareTag("Left")) {
-            Destroy(this.gameObject);
+      Destroy(this.gameObject);
     }
   }
 }
